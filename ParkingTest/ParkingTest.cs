0512ca9ebcfd373c96856fc533dc4d413c3f8413
chonmein <chonmein@gmail.com>
2021-07-31 10:08:50 +0800

    11到30分鐘_回覆2元，撰寫不會通過的單元測試

--- ParkingTest/ParkingTest.cs
+++ ParkingTest/ParkingTest.cs
@@ -19,6 +19,16 @@ namespace ParkingTest
             Assert.AreEqual(expected,actual);
         }
 
+        [TestCase(11)]
+        [TestCase(30)]
+        public void GetParkingFee_11到30分鐘_回覆2元(int minutes)
+        {
+            Parking parking = new Parking();
+            var expected = 2;
+            var actual = parking.GetParkingFee(minutes);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 
     public class Parking
