f272007773236334f9656c06ab94bc1667166dc5
chonmein <chonmein@gmail.com>
2021-07-31 10:00:40 +0800

    小於10分鐘_回覆0元，撰寫測試失敗的單元測試

--- ParkingTest/ParkingTest.cs
+++ ParkingTest/ParkingTest.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace ParkingTest
 {
     public class ParkingTest
     {
-        
+        [Test]
+        public void GetParkingFee_小於10分鐘_回覆0元()
+        {
+            Parking parking = new Parking();
+            var expected = 0;
+            var actual = parking.GetParkingFee(9);
+            
+            Assert.AreEqual(expected,actual);
+        }
 
+    }
+
+    public class Parking
+    {
+        public int GetParkingFee(int minutes)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
